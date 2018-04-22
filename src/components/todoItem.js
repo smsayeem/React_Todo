c29cca253206f42b6d5e21ef3fea344d3da44779
Sayeem Sadot <smsayeem@yahoo.com.au>
2018-04-23 08:25:31 +1000

    first commit

---
+++ src/components/todoItem.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './todoItem.css';
+
+class TodoItem extends React.Component {
+    constructor(props){
+        super(props);
+    }
+
+    removeTodo(id) {
+        this.props.removeTodo(id)
+    }
+
+    render(){
+        return(
+            <div className="todoWrapper">
+                <button className="removeTodo" onClick={(e)=>this.removeTodo(this.props.id)} >Remove</button> 
+                {this.props.todo.text}               
+
+            </div>
+        );
+    }
+
+
+}
+
+export default TodoItem;
